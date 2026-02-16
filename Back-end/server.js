911e271b70b4e3fa824bd0cc59ff43e58452257f
the000000 <darkking7972@gmail.com>
2026-02-16 11:17:31 +0700

    update v2

--- Back-end/server.js
+++ Back-end/server.js
@@ -33,6 +33,4 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/courses", require("./routes/materialRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`✅ Server running on http://localhost:${PORT}`),
-);
+app.listen(PORT, () => console.log(`✅ Server running`));
